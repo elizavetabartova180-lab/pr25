2155f516d2c5109bae4decf5c7a480eadf7cc6e0
Kik02 <Golovin.Savelii@yandex.ru>
2026-03-19 09:28:06 +0500

    Разработка общего класса. Бартова

---
+++ Shop_Bartova/Classes/Common/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Shop_Bartova.Classes.Common
+{
+   public class DBConnection
+    {
+        public static string Path = @"C:\Users\student-A502.PERMAVIAT\Downloads\Shop_Bartovanew-master\Shop_Bartovanew-master\Shop_Bartova\DB.accdb";
+        public static OleDbConnection Connection()
+        {
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + Path);
+            oleDbConnection.Open();
+            return oleDbConnection;
+        }
+        public static OleDbDataReader Query(string sql, OleDbConnection connection)
+        {
+            return new OleDbCommand(sql, connection).ExecuteReader();
+        }
+        public static void CloseConnection(OleDbConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
