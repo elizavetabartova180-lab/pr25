cd43b4fc61416a92c06cb91a2e821d7082376419
Kik02 <Golovin.Savelii@yandex.ru>
2026-03-19 10:45:01 +0500

    Разработанный контекст для детских товаров. Бартова

--- Shop_Bartova/Models/Children.cs
+++ Shop_Bartova/Models/Children.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Shop_Bartova.Models
 {
     public class Children : Shop
     {
-        public int Age { get; set; }
-        public Children(string name, int price, int age) : base(name, price)
+        public int Age;
+        public int IdShop;
+        public Children() { }
+        public Children(int id, string name, int price, int age, int idShop) : base(id, name, price)
         {
             this.Age = age;
+            this.IdShop = idShop;
         }
     }
 }
